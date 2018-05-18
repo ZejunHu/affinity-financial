0e229128ae2e9c7fe6ddd9be9d53c496986cc8f1
Jonathan <jonathanzejunhu@gmail.com>
2018-05-18 11:25:28 -0400

    change some style, fix navbar logo in mobile device

--- keystone.js
+++ keystone.js
@@ -65,7 +65,7 @@ keystone.set('routes', require('./routes'));
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
 	posts: ['posts', 'post-categories'],
-	enquiries: 'enquiries',
+	enquiries: ['enquiries', 'contacts'],
 	users: 'users',
 });
 
